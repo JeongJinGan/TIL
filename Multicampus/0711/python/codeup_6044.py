8c574e59505bbc89136962f021eba076f7c8d11d
JeongJinGan <rksaudwls123@naver.com>
2022-07-12 17:57:57 +0900

    update codeup

---
+++ Multicampus/0711/python/codeup_6044.py
@@ -0,0 +1,7 @@
+a,b = input().split()
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)//int(b))
+print(int(a)%int(b))
+print(format(float(a)/float(b),".2f"))
---
+++ Multicampus/0711/python/codeup_6045.py
@@ -0,0 +1,4 @@
+a,b,c = input().split()
+sum = int(a) + int(b) + int(c)
+avg = float(a) + float(b) + float(c)
+print(sum, format(avg/3,".2f"))
