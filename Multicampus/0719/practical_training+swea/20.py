b2ead5f86690eed89e8a72f813d44226f57f5468
JeongJinGan <rksaudwls123@naver.com>
2022-07-19 18:29:32 +0900

    update 0719

--- Multicampus/0719/practical_training+swea/20.py
+++ Multicampus/0719/practical_training+swea/20.py
@@ -1,6 +1,17 @@
-number = 2022
+number = 123456
 num = 0
 while number > 0:
     num += number%10
     number //=10
 print(num)
+
+
+# divmod(x,y)는 x를 y로 나누고,
+# 결과를 tuple로 반환
+# (몫, 나머지)
+# 다른방법
+# answer = 0
+# while number > 0:
+#     number, remainder = divmod(number, 10)
+#     answer += remainder
+# print(answer)
