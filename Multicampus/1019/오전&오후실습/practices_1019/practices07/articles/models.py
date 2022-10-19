355ac866d14c50a30f5f74581fb8a311c2997275
JeongJinGan <rksaudwls123@naver.com>
2022-10-19 17:25:46 +0900

    create 1019

---
+++ Multicampus/1019/오전&오후실습/practices_1019/practices07/articles/models.py
@@ -0,0 +1,33 @@
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from imagekit.processors import Thumbnail
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+"""
+게시판 기능 
+- 제목(20글자이내)
+- 내용(긴 글)
+- 글 작성시간/수정시간(자동으로 기록, 날짜/시간)
+"""
+# 1. 모델 설계 (DB 스키마 설계)
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=True)
+    thumbnail = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
